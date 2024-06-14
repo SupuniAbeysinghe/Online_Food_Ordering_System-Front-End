b389d71da56f1f692fa5eb49d22555affce2a2fe
SupuniAbeysinghe <supuniab1@gmail.com>
2024-06-14 23:56:53 +0530

    dark theme created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar.jsx';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Theme/DarkTheme.js';
 
 function App() {
     return ( <
         div >
+        <
+        ThemeProvider theme = { darkTheme }
+        />
+
+        <
+        CssBaseline / >
         <
         Navbar / >
 
