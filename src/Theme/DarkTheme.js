c4ab335d2d27d5562c4dc28bdb482c179f093d9e
SupuniAbeysinghe <supuniab1@gmail.com>
2024-06-16 19:46:55 +0530

    implemented tha navbar

--- src/Theme/DarkTheme.js
+++ src/Theme/DarkTheme.js
@@ -10,12 +10,12 @@ export const darkTheme = createTheme({
             main: "#5A20CB"
         },
         black: {
-            main: "#242B2E"
+            main: "#0D0D0D"
         },
         background: {
-            main: "#000000",
+            main: "#0000000",
             default: "#0D0D0D",
-            PAPER: "0D0D0D"
+            PAPER: "#0D0D0D"
         },
         textColor: {
             main: "#111111"
