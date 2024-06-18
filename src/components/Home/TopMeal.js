4bf88908b16e7da3810b0a814320cb4118910c47
SupuniAbeysinghe <supuniab1@gmail.com>
2024-06-18 23:45:56 +0530

    add the slide

---
+++ src/components/Home/TopMeal.js
@@ -0,0 +1,51 @@
+const topMeal = [{
+        image: "https://cdn.pixabay.com/photo/2017/08/06/06/43/pizza-2589575_1280.jpg",
+        title: "Pizza"
+
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2023/10/08/13/03/ai-generated-8302143_1280.jpg",
+        title: "Burger"
+
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2017/05/10/17/27/sandwich-2301387_1280.jpg",
+        title: "Sandwich"
+
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2022/06/23/09/38/chicken-7279386_1280.jpg",
+        title: "Chicken"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/1410130688/photo/mutton-biryani-served-in-a-golden-dish-isolated-on-dark-background-side-view-indian-food.jpg?s=612x612&w=0&k=20&c=ueFrghYZuKAty-rFVe5dtMtNIbn0jaUsSvCUwTVOmd8=",
+        title: "Biriyani"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/594065052/photo/beef-pad-thai-and-chicken-satay-dinner-viewed-from-above.jpg?s=612x612&w=0&k=20&c=KyKe2BMQiOpL1Ke1WYYTeFCKzJtY5PPOLRpgTTZUDdU=",
+        title: "Thai"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/466724890/photo/healthy-homemade-carrot-cake.jpg?s=612x612&w=0&k=20&c=Qt89yzcMCq8awNO_EhJJwdBOp3yyJA2YYG01D7ZL3gA=",
+        title: "Cake"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/585303220/photo/sprinkles.jpg?s=612x612&w=0&k=20&c=WOiT7UM38wLFUr6OV34O3uvN6e4_aoIAaypkgG_7ze0=",
+        title: "Cupcake"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/1292564353/photo/assorted-indian-food-on-wooden-background-paneer-tikka-dal-tadka-jeera-rice-roti-dishes-and.jpg?s=612x612&w=0&k=20&c=zPg4tE9Wt5f59UbTYyuf8IhEf4uw4YziC4BjILWgurs=",
+        title: "North Indian Food"
+
+    },
+    {
+        image: "https://media.istockphoto.com/id/1325172440/photo/spaghetti-alla-puttanesca-italian-pasta-dish-with-tomatoes-black-olives-capers-anchovies-and.jpg?s=612x612&w=0&k=20&c=ieMxGg7flhSltOMDpuLZINAWYT2W2WDjJTlwoUWuwH4=",
+        title: "Noodles"
+
+    },
+]
